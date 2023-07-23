c572fb8fa8c269ea878c8e8290c4e0a7b7daab4c
mazigodswill <godswillnathaniel97@gmail.com>
2023-07-23 01:08:28 -0700

    checks if : is in the current directory

---
+++ string_fn3.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_cdir - checks if ":" is in the current directory
+ * @path: character pointer
+ * @i: type integer pointer of index
+ * Return: 1 if the path is searchable in the cd, 0 otherwise
+ */
+
+int is_cdir(char *path, int *i)
+{
+	if (path[*i] == ':')
+		return (1);
+
+	while (path[*i] != ':' && path[*i])
+		*i += 1;
+
+	if (path[*i])
+		*i += 1;
+
+	return (0);
+}
